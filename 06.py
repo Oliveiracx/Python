893b535e7b856e3cd997bca98fdd62bbba3b17ae
Raphael de Oliveira <s4ntos.raphael@gmail.com>
2024-08-16 20:53:33 -0300

    Área do triangulo (Finalizada)

---
+++ 06.py
@@ -0,0 +1,8 @@
+print ("Vamos calcular a área de um retangulo")
+print ()
+print ("Escreva o valor da base do seu retangulo")
+base = int(input())
+print ("agora escreva o valor da altura")
+altura = int(input())
+area = base*altura
+print ("A área do seu retangulo é {}".format(area))
