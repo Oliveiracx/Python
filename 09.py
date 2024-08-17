ec8e469aa4b1f99f459bffca39380d577ff601d8
Raphael de Oliveira <s4ntos.raphael@gmail.com>
2024-08-16 21:22:08 -0300

    Salário (Finalizada)

---
+++ 09.py
@@ -0,0 +1,13 @@
+print ("Bem vindo ao portal do salário Brasileiro!!")
+nome = input("Insira seu nome completo: ")
+print ("----------------------------")
+print ("Verificando seu salário atual... ")
+print ("----------------------------")
+salario_in = 1500
+print ("Seu salário atual é R${}".format(salario_in))
+print ("----------------------------")
+print ("O percentual de reajuste é 10% logo:")
+salario_final = int
+salario_final = 1500/10+1500
+print ("----------------------------")
+print ("Seu salário atual é R${}".format(salario_final))
