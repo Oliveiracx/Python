a3f2f67f79b264d1881cfdd2990400309cc8432f
Raphael de Oliveira <s4ntos.raphael@gmail.com>
2024-08-16 21:03:11 -0300

    Antecessor (Finalizada)

--- 05.py
+++ 05.py
@@ -2,4 +2,4 @@ print ("Escreva um número:")
 numero = int(input())
 antecessor = numero-1
 print ("Seu número é {}".format(number))
-print ("Seu antecessor é {}".format(antecessor))
+print ("Seu antecessor é {}".format(antecessor)
