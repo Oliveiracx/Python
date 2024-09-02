5c0199759324462db2b0fad3b10b2a4af8d8c7f1
Raphael de Oliveira <s4ntos.raphael@gmail.com>
2024-09-02 20:26:08 -0300

    Verificação de número positivo ou negativo

---
+++ 15.py
@@ -0,0 +1,7 @@
+numero = int(input("Escreva um numero positivo ou negativo:"))
+if numero < 0 :
+    print("Seu número é negativo")
+if numero > 0 :
+    print("Seu número é positivo")
+if numero == 0 :
+    print("Seu número é positivo")
