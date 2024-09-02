ab49af65495449dd27387574cf4780d08cbced3b
Raphael de Oliveira <s4ntos.raphael@gmail.com>
2024-09-02 20:01:09 -0300

    Verificação do número 10

---
+++ 14.py
@@ -0,0 +1,7 @@
+valor = int(input("Escreva um valor:"))
+if valor > 10:
+    print("Seu número é maior que 10!!")
+if valor < 10:
+    print("Seu número é menor que 10!!")
+if valor == 10:
+    print("Seu número é o 10!!")
