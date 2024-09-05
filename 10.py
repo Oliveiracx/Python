3fb9c6f44453cb165561576ddc84614148305044
Raphael de Oliveira <s4ntos.raphael@gmail.com>
2024-09-04 21:40:27 -0300

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,12 +1,1 @@
-i_distribuidor = float
-impostos = float
-valor_final = float
-print("Bem vindo a calculadora de preço de mercado!!")
-fabricação = float(input("Digite o valor da fabricação do veiculo:R$"))
-print("_"*50)
-print("O valor de fabricação so seu carro é R${}".format(fabricação))
-i_distribuidor = (fabricação)*0.28
-impostos = (fabricação)*0.45
-valor_final = (fabricação+i_distribuidor+impostos)
-print("O valor cobrado ao consumidor é R${}".format(valor_final))
-print("_"*50)
+1
