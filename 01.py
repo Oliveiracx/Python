2d5f182a547f592d34449c5bfd35fbe475f28992
Raphael de Oliveira <s4ntos.raphael@gmail.com>
2024-09-04 21:32:48 -0300

    Permutação de Variáveis

--- 01.py
+++ 01.py
@@ -1,1 +1,13 @@
-1
+A = int(10)
+B = int(20)
+print ("A variavel A tem o valor de {}".format(A))
+print ("e a variavel B tem o valor de {}".format(B))
+aux1 = A
+aux2 = B
+A = B
+B = aux1
+print ()
+print ("Agora podemos trocar elas assim")
+print ()
+print ("A variavel A tem o valor de {}".format(A))
+print ("e a variavel B tem o valor de {}".format(B))
