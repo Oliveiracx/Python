eff5c2dd8d368fa91dcde52c0f4ac90f28db86a0
Raphael de Oliveira <s4ntos.raphael@gmail.com>
2024-09-04 21:36:30 -0300

    Update 06.py

--- 06.py
+++ 06.py
@@ -1,8 +1,1 @@
-print ("Vamos calcular a área de um retangulo")
-print ()
-print ("Escreva o valor da base do seu retangulo")
-base = int(input())
-print ("agora escreva o valor da altura")
-altura = int(input())
-area = base*altura
-print ("A área do seu retangulo é {}".format(area))
+1
